1ce634ffb5b28fcc8fd1322ff71226054ae61ef0
agiatikazinat <robotproject333@gmail.com>
2024-04-03 19:22:05 -0400

    .

---
+++ program/left_arm/left_arm.ino
@@ -0,0 +1,16 @@
+#include <servo.h>
+
+#define POS_LSY A0;
+#define POS_LSX A1;
+#define POS_LSZ A3;
+#define POS_LBI A2;
+
+void setup() {
+  Serial.begin(9600);
+
+
+}
+
+void loop() {
+  
+}
---
+++ program/left_arm/library.h
@@ -0,0 +1,12 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H 
+
+#include <Arduino.h>
+
+
+
+
+
+
+
+#endif
