196f9919cf37e49f9f0a57ad6724f6246e578242
agiatikazinat <robotproject333@gmail.com>
2024-04-03 23:25:08 -0400

    start left arm

--- program/left_arm/library.h
+++ program/left_arm/library.h
@@ -2,9 +2,26 @@
 #define LIBRARY_H 
 
 #include <Arduino.h>
+#include "library.cpp"
 
+class Motor{
+    private:
+        int en;
+        int in1;
+        int in2;
+        int potPin;
+        int current;
+        int speed;
+        int const delta = 2;
+        int direction = 1;
 
-
+    public:
+        Motor(int en, int in1, int in2, int potPin);
+        int current();
+        void move(int degree);
+        void setSpeed(int speed);
+        void stopMotor();
+}
 
 
 
