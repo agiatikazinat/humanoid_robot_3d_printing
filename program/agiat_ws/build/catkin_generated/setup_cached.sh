88a4f6252ee57282bc4a12f35fbd3c8beae004f0
agiatikazinat <robotproject333@gmail.com>
2024-04-07 22:25:24 -0400

    start urdf

---
+++ program/agiat_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ikazinatagiat/Documents/Agiat_Ikazinat/program/agiat_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/ikazinatagiat/Documents/Agiat_Ikazinat/program/agiat_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ikazinatagiat/Documents/Agiat_Ikazinat/program/agiat_ws/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ikazinatagiat/Documents/Agiat_Ikazinat/program/agiat_ws/src:$ROS_PACKAGE_PATH"
