e568fe64b979080e0129a22845a3feccdb8574d9
agiatikazinat <robotproject333@gmail.com>
2024-04-12 14:03:27 -0400

    .

--- experiment/testing_motor_drive/testing_motor_drive.ino
+++ experiment/testing_motor_drive/testing_motor_drive.ino
@@ -2,26 +2,51 @@
 #include <Servo.h>
 
 Servo servo;
-AF_DCMotor motor(1, MOTOR12_8KHZ);
+AF_DCMotor motor(2, MOTOR12_64KHZ);
 int val = 100;
+int target = 240;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   motor.setSpeed(200);
   servo.attach(9);
+  int current = map(analogRead(A0), 0, 1023, 0, 270);
+  Serial.println(current);
+  motor.run(FORWARD);
+  delay(2000);
+  motor.run(RELEASE);
+  delay(1000);
+  Serial.println(map(analogRead(A0), 0, 1023, 0, 270));
+  
 
 }
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-  motor.run(FORWARD);
-  delay(1000);
-  motor.run(BACKWARD);
-  delay(1000);
-  motor.run(RELEASE);
-  delay(1000);
+  int current = map(analogRead(A0), 0, 1023, 0, 270);
+//  if (current < target - 2){
+//    motor.run(FORWARD);
+//  }
+//  else if (current > target + 2){
+//    motor.run(BACKWARD);
+//  }
+//  else {
+//    motor.run(RELEASE);
+//  }
 
-  servo.write(val);
+//  motor.run(FORWARD);
+//  delay(1000);
+//  motor.run(RELEASE);
+//  delay(1000);
+//  Serial.println(current);
+//  motor.run(BACKWARD);
+//  delay(1000);
+//  motor.run(RELEASE);
+//  delay(1000);
+  Serial.println(current);
+  delay(1000);
+//
+//  servo.write(val);
 }
