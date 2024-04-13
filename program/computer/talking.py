f765c76e753dc6a9ae42b05f8b589095ab42afb0
agiatikazinat <robotproject333@gmail.com>
2024-04-13 13:07:24 -0400

    playsounds adding

---
+++ program/computer/talking.py
@@ -0,0 +1,16 @@
+from playsound import playsound 
+import os                 
+import pyaudio 
+import JarvisAI
+import pyttsx3 
+import speech_recognition as sr   
+from gtts import gTTS
+import sys
+import json 
+
+
+
+
+    
+
+
